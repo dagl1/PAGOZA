1154c2c0bb7d8f286465319c5bf908cc2d3bfff1
dagl1 <jellebonthuis@hotmail.com>
2026-03-23 22:56:46 +0100

    feat(site/demo): added swiss_roll.png for proof of concept

--- src/demo/app.py
+++ src/demo/app.py
@@ -1,10 +1,23 @@
+from pathlib import Path
+
 import dash
 from dash import html
 
-app = dash.Dash(__name__)
+ROOT_DIR = Path(__file__).resolve().parents[2]
+ASSETS_DIR = ROOT_DIR / "assets"
+
+
+app = dash.Dash(__name__, assets_folder=str(ASSETS_DIR))
 server = app.server  # 👈 REQUIRED for Render
 
-app.layout = html.Div("Hello from PAGOZA!")
+
+# add swiss_roll.png to show in div
+layout = html.Div(
+    html.Img(
+        src=app.get_asset_url("swiss_roll.png"), style={"width": "100%", "height": "auto"}
+    )
+)
+app.layout = layout
 
 if __name__ == "__main__":
     app.run(debug=True)
